f42cf60d8b688b8e31dce22b44f24cca6c50c0b7
orion-tux <alex.sejnost@pm.me>
2024-07-08 13:05:42 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,43 @@
+minetest.register_privilege("gamemode", {
+    description = "Changes player's gamemode",
+    give_to_singleplayer = true,
+    give_to_admin = true,
+})
+
+minetest.register_chatcommand("gamemode", {
+    description = "Set '1' for creative and '0' for survival",
+    privs = {
+        gamemode = true
+    },
+    func = function (name, param)
+        local privs = minetest.get_player_privs(name)
+        if param == "1" then
+            privs.creative = true
+            minetest.set_player_privs(name, privs)
+            return true, "gm 1"
+        elseif param == "0" then
+            privs.creative = nil
+            minetest.set_player_privs(name, privs)
+            return true, "gm 0"
+        end
+    end
+})
+
+minetest.register_chatcommand("gm", {
+    description = "Set '1' for creative and '0' for survival",
+    privs = {
+        gamemode = true
+    },
+    func = function (name, param)
+        local privs = minetest.get_player_privs(name)
+        if param == "1" then
+            privs.creative = true
+            minetest.set_player_privs(name, privs)
+            return true, "gm 1"
+        elseif param == "0" then
+            privs.creative = nil
+            minetest.set_player_privs(name, privs)
+            return true, "gm 0"
+        end
+    end
+})
